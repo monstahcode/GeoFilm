f3302111794a3f12c985b7411e09c829453c2a50
monstahcode <ivanag2005ag@gmail.com>
2025-05-28 10:40:27 +0200

    Spring project

---
+++ src/main/java/com/metrica/marzo25/geofilm/GeofilmApplication.java
@@ -0,0 +1,13 @@
+package com.metrica.marzo25.geofilm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeofilmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeofilmApplication.class, args);
+	}
+
+}
