0506e5350f3e7a789f4e277198684c58a7b2d893
monstahcode <ivanag2005ag@gmail.com>
2025-05-29 11:48:02 +0200

    fix

--- src/main/java/com/metrica/marzo25/geofilm/repository/UserRepository.java
+++ src/main/java/com/metrica/marzo25/geofilm/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.metrica.marzo25.geofilm.repository;
 
 import com.metrica.marzo25.geofilm.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -9,13 +11,13 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    Optional<User> findByUsername(String username);
+	Optional<User> findByUsername(String username);
 
-    Optional<User> findByEmail(String email);
-  
-  	@Query(value = "SELECT u FROM User u WHERE u.email = :email AND u.password = :password")
-	  User loginWithEmailAndPassword(@Param("email") String email, @Param("password") String password);
-	
-	  @Query(value = "INSERT INTO User u (u.email, u.password, u.username) VALUES (:email , :password , :username)")
-	  User registerUser(@Param("email") String email, @Param("password") String password, @Param("username") String username);
+	Optional<User> findByEmail(String email);
+
+	@Query(value = "SELECT u FROM User u WHERE u.email = :email AND u.password = :password")
+	User loginWithEmailAndPassword(@Param("email") String email, @Param("password") String password);
+
+	@Query(value = "INSERT INTO User u (u.email, u.password, u.username) VALUES (:email , :password , :username)")
+	User registerUser(@Param("email") String email, @Param("password") String password, @Param("username") String username);
 }
